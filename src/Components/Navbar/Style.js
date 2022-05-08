0713f0e247c0802142a4f061460aaf69f0fc00a0
Dolagy1199 <dolagydolagy1199@gmail.com>
2022-05-08 17:03:40 +0200

    add

---
+++ src/Components/Navbar/Style.js
@@ -0,0 +1,12 @@
+
+// //import {makeStyles} from '@mui/material'
+// import {makeStyles} from '@mui/material'
+
+
+
+// const usestyle = makeStyles((theme) => ({
+//     container: {
+//         backgroundColor: theme.palette.backgroundColor.pages,
+//     }
+// }));
+// export default usestyle;
