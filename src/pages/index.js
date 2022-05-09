3864976ce29bb415fde3182d9fa5ee1dff36806f
Dolagy1199 <dolagydolagy1199@gmail.com>
2022-05-09 20:25:22 +0200

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import PersistentDrawerRight from "../Components/Navbar/Navbar";
 import { StyledEngineProvider } from "@mui/material/styles";
+import Home from '../Components/Home/Home'
 const PagesRoutes = () => {
     return (
         <StyledEngineProvider injectFirst>
             <PersistentDrawerRight>
                 <Switch>
+                    <Route exact path="/" component={Home} />
                 </Switch>
             </PersistentDrawerRight>
         </StyledEngineProvider>
